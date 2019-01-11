a0e4088d017a86845ab8bb69061001b4a3ed06ed
KacobJeith <jacobkeith42@gmail.com>
2019-01-11 13:15:33 -0500

    Save off annotations to the realtime database

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,6 @@ var admin = require("firebase-admin");
 
 admin.initializeApp(functions.config().firebase);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
 exports.processVideo = functions.https.onRequest(async function (_request, response) {
 	console.log('Beginning video process...');
   	const video = new Video.VideoIntelligenceServiceClient();
@@ -17,7 +13,6 @@ exports.processVideo = functions.https.onRequest(async function (_request, respo
  	const request = {
  	  inputUri: gcsUri,
  	  features: ['OBJECT_TRACKING'],
- 	  //recommended to use us-east1 for the best latency due to different types of processors used in this region and others
  	  locationId: 'us-east1',
  	};
 
@@ -26,64 +21,18 @@ exports.processVideo = functions.https.onRequest(async function (_request, respo
  	// Detects objects in a video
  	const [operation] = await video.annotateVideo(request);
  	const results = await operation.promise();
- 	console.log(results);
 
  	//Gets annotations for video
  	const annotations = results[0].annotationResults[0];
  	
+ 	saveResultsToDatabase(annotations);
 
- 	// response.send("Attempting to process the video! \n\n", JSON.stringify(results, null, 2));
+ 	response.send("Attempting to process the video!");
 });
 
-const processVideo = (req, res) => {
-
+const saveResultsToDatabase = (annotations) => {
+	admin.database().ref('results').set(annotations);
+					
 }
 
-const logResults = (results) => {
-
-	const objects = annotations.objectAnnotations;
- 	objects.forEach(object => {
- 	  console.log(`Entity description:  ${object.entity.description}`);
- 	  console.log(`Entity id: ${object.entity.entityId}`);
- 	  const time = object.segment;
- 	  if (time.startTimeOffset.seconds === undefined) {
- 	    time.startTimeOffset.seconds = 0;
- 	  }
- 	  if (time.startTimeOffset.nanos === undefined) {
- 	    time.startTimeOffset.nanos = 0;
- 	  }
- 	  if (time.endTimeOffset.seconds === undefined) {
- 	    time.endTimeOffset.seconds = 0;
- 	  }
- 	  if (time.endTimeOffset.nanos === undefined) {
- 	    time.endTimeOffset.nanos = 0;
- 	  }
- 	  console.log(
- 	    `Segment: ${time.startTimeOffset.seconds}` +
- 	      `.${(time.startTimeOffset.nanos / 1e6).toFixed(0)}s to ${
- 	        time.endTimeOffset.seconds
- 	      }.` +
- 	      `${(time.endTimeOffset.nanos / 1e6).toFixed(0)}s`
- 	  );
- 	  console.log(`Confidence: ${object.confidence}`);
- 	  const frame = object.frames[0];
- 	  const box = frame.normalizedBoundingBox;
- 	  const timeOffset = frame.timeOffset;
- 	  if (timeOffset.seconds === undefined) {
- 	    timeOffset.seconds = 0;
- 	  }
- 	  if (timeOffset.nanos === undefined) {
- 	    timeOffset.nanos = 0;
- 	  }
- 	  console.log(
- 	    `Time offset for the first frame: ${timeOffset.seconds}` +
- 	      `.${(timeOffset.nanos / 1e6).toFixed(0)}s`
- 	  );
- 	  console.log(`Bounding box position:`);
- 	  console.log(`\tleft   :${box.left}`);
- 	  console.log(`\ttop    :${box.top}`);
- 	  console.log(`\tright  :${box.right}`);
- 	  console.log(`\tbottom :${box.bottom}`);
- 	});
-}
 
