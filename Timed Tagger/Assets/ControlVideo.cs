b9cf94f030cd85220e531519f91f4fac20cdba8b
dfashbaugh <dylan.fashbaugh@gmail.com>
2019-03-06 00:07:54 -0500

    Added file browser and video control

---
+++ Timed Tagger/Assets/ControlVideo.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class ControlVideo : MonoBehaviour
+{
+    public Text videoTime;
+    public Text playbackSpeed;
+
+    VideoPlayer theVideoPlayer;
+
+    public void LoadNewVideo(string filePath)
+    {
+        theVideoPlayer.url = filePath;
+    }
+
+    public void SkipAhead(float numberOfSeconds)
+    {
+        theVideoPlayer.time += numberOfSeconds;
+    }
+
+    public void SkipBack(float numberOfSeconds)
+    {
+        theVideoPlayer.time -= numberOfSeconds;
+    }
+
+    public void IncreasePlaybackSpeed()
+    {
+        theVideoPlayer.playbackSpeed++;
+    }
+
+    public void PauseVideo()
+    {
+        theVideoPlayer.playbackSpeed = 0;
+    }
+
+    public void PlayVideo()
+    {
+        theVideoPlayer.playbackSpeed = 1;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theVideoPlayer = GetComponent<VideoPlayer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            theVideoPlayer.playbackSpeed++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            theVideoPlayer.playbackSpeed = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            theVideoPlayer.time -= 5;
+        }
+
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            theVideoPlayer.playbackSpeed = 0;
+        }
+
+        videoTime.text = theVideoPlayer.time.ToString();
+        playbackSpeed.text = theVideoPlayer.playbackSpeed.ToString() + "x";
+    }
+}
---
+++ Timed Tagger/Assets/Video Control UI Scripts/GetVideoPath.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using SimpleFileBrowser;
+
+public class GetVideoPath : MonoBehaviour, IPointerClickHandler
+{
+    public ControlVideo videoController;
+
+    IEnumerator ShowLoadDialogCoroutine()
+    {
+        // Show a load file dialog and wait for a response from user
+        // Load file/folder: file, Initial path: default (Documents), Title: "Load File", submit button text: "Load"
+        yield return FileBrowser.WaitForLoadDialog(false, null, "Load File", "Load");
+
+        // Dialog is closed
+        // Print whether a file is chosen (FileBrowser.Success)
+        // and the path to the selected file (FileBrowser.Result) (null, if FileBrowser.Success is false)
+        Debug.Log(FileBrowser.Success + " " + FileBrowser.Result);
+
+        if(FileBrowser.Success)
+        {
+            videoController.LoadNewVideo(FileBrowser.Result);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        StartCoroutine(ShowLoadDialogCoroutine());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
