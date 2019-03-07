4ce9c3a4d0e4b4d3e01e4136c1631d2225dcde26
dfashbaugh <dylan.fashbaugh@gmail.com>
2019-03-07 00:09:24 -0500

    PL-271 Add toggle button

---
+++ Timed Tagger/Assets/ToggleButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ToggleButton : MonoBehaviour, IPointerClickHandler
+{
+    public bool Toggled;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (Toggled)
+            Toggled = false;
+        else
+            Toggled = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Toggled)
+        {
+            GetComponent<Image>().color = new Color(0.2f, 0.8f, 0.2f);
+        }
+        else
+        {
+            GetComponent<Image>().color = new Color(1,1,1);
+        }
+    }
+}
