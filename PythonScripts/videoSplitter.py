bfcaad22ac29e9bea8f3a4388cd3a7c32faaea84
Jacob Keith <jacobkeith42@gmail.com>
2019-03-11 20:46:01 -0400

    PL-298 add Dylans csv parser, and merge with video splitter

--- PythonScripts/videoSplitter.py
+++ PythonScripts/videoSplitter.py
@@ -4,24 +4,51 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--video', type=str, required=True, help='video file')
+parser.add_argument('--csv', type=str, required=True, help='csv label file')
 args = parser.parse_args()
 
-vidcap = cv2.VideoCapture(args.video)
-success,image = vidcap.read()
-count = 0
+class BusData:
+	def __init__(self, realTime, relativeTime, stateNumber, stateName) :
+		self.realTime = realTime
+		self.relativeTime = relativeTime
+		self.stateNumber = stateNumber
+		self.stateName = stateName
 
-filename = args.video.split('.')[0]
+def getCSVData() :
 
-print('Processing ', filename)
+  fname = args.csv
 
-folder = "./{}".format(filename)
+  BusDataList = []
 
-if (os.path.isdir(folder) == False) :
-    os.mkdir(folder)
+  with open(fname, 'r') as f:
+      content = f.readlines()
+      for x in range(0, len(content)) :
+        splitValue = content[x].split(',')
+        if(len(splitValue) == 4) :
+          BusDataList.append(BusData(splitValue[0], float(splitValue[1]), int(splitValue[2]), splitValue[3]))
+  
+  return BusDataList
 
-while success:
-  savepath = "./{}/frame_{}.jpg".format(filename, count)
-  cv2.imwrite(savepath, image)     # save frame as JPEG file      
+def readVideo() :
+  
+  vidcap = cv2.VideoCapture(args.video)
   success,image = vidcap.read()
-#   print('Read a new frame: ', success)
-  count += 1
+
+  filename = args.video.split('.')[0]
+  print('Watching ', filename)
+
+  folder = "./{}".format(filename)
+
+  if (os.path.isdir(folder) == False) :
+      os.mkdir(folder)
+
+  count = 0
+  while success:
+    savepath = "./{}/frame_{}.jpg".format(filename, count)
+    # cv2.imwrite(savepath, image)     # save frame as JPEG file      
+    success,image = vidcap.read()
+  #   print('Read a new frame: ', success)
+    count += 1
+
+
+readVideo()
