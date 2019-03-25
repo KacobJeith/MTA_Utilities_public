dc9a1e3b519a41251618df258e2beedae28246a0
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2019-03-25 18:25:31 -0400

    PL-315 Added better state handling and reporting

--- StateMachineSystem/PythonCode/FindStates.py
+++ StateMachineSystem/PythonCode/FindStates.py
@@ -7,9 +7,29 @@ class SingleObstruction:
 		self.state = state
 		self.curTime = curTime
 
+class ObstructionPeriod : 
+	def __init__(self, severity, startTime, endTime, visionStates) :
+		self.severity = severity
+		self.startTime = startTime
+		self.endTime = endTime
+		self.visionStates = visionStates
+
 class MotionState:
+	#Motion State Enumeration
+	Stopped = 0
+	Accelerating = 1
+	Cruising = 2
+	Decelerating = 3
+
 	def __init__(self, stateType, transitionFunction) :
 		self.stateType = stateType
+		self.transitionFunction = transitionFunction
+
+	def RunStateMachine(self, acceleration, speed) :
+		return self.transitionFunction(acceleration, speed)
+
+	def GetStateType(self) :
+		return self.stateType
 
 fname = sys.argv[1]
 
@@ -40,48 +60,144 @@ with open(fname, 'r') as f:
 
 
 ## Capture The Information
-lastState = states[0]
-inObstruction = False
+# lastState = states[0]
+# inObstruction = False
 
+# currentSeverity = 0
+
+# singleObstructionEventList = []
+# allObstructions = []
+
+# for x in range(1, len(speeds)) :
+# 	acceleration = speeds[x] - speeds[x-1]
+
+# 	if acceleration > 0 :
+# 		if inObstruction :
+# 			singleObstructionEventList.append(SingleObstruction(currentSeverity, states[x], times[x]))
+# 			allObstructions.append(singleObstructionEventList)
+
+# 		inObstruction = False
+# 		currentSeverity = 0
+# 		singleObstructionEventList = []
+
+# 	else : # Detecting an obstruction
+		
+# 		currentSeverity += 1
+
+# 		if states[x] != lastState or not inObstruction :
+# 			singleObstructionEventList.append(SingleObstruction(currentSeverity, states[x], times[x]))
+
+# 		lastState = states[x]
+
+# 		if not inObstruction :
+# 			inObstruction = True
+		
+
+## Print Things
+# for x in range(0, len(allObstructions)) :
+# 	toPrintStr = ""
+# 	for y in range(0, len(allObstructions[x])) :
+# 		toPrintStr += allObstructions[x][y].curTime + ",  " + str(allObstructions[x][y].severity) + "," + str(allObstructions[x][y].state) + ":  "
+# 		print(toPrintStr)
+
+
+def StoppedTransitionFunction(acceleration, speed) :
+	if acceleration > 0 :
+		return MotionState.Accelerating
+
+def AcceleratingTransitionFunction(acceleration, speed) :
+	if acceleration < 0 :
+		return MotionState.Decelerating
+	elif acceleration == 0 :
+		return MotionState.Cruising
+
+def DeceleratingTransitionFunction(acceleration, speed) :
+	if acceleration > 0 :
+		return MotionState.Accelerating
+	elif acceleration == 0 and speed > 0 :
+		return MotionState.Cruising
+	elif acceleration == 0 and speed == 0 :
+		return MotionState.Stopped
+
+def CruisingTransitionFunction(acceleration, speed) :
+	if acceleration < 0 :
+		return MotionState.Decelerating
+	elif acceleration > 0 :
+		return MotionState.Accelerating
+
+StoppedState = MotionState(MotionState.Stopped, StoppedTransitionFunction)
+AcceleratingState = MotionState(MotionState.Accelerating, AcceleratingTransitionFunction)
+DeceleratingState = MotionState(MotionState.Decelerating, DeceleratingTransitionFunction)
+CruisingState = MotionState(MotionState.Cruising, CruisingTransitionFunction)
+
+## Use State Machine
 currentSeverity = 0
 
-singleObstructionEventList = []
 allObstructions = []
+
+obstructionStartTime = ""
+obstructionVisionStates = []
+
+if speeds[0] > 0 :
+	currentMotionState = CruisingState
+else :
+	currentMotionState = StoppedState
 
 for x in range(1, len(speeds)) :
 	acceleration = speeds[x] - speeds[x-1]
 
-	if acceleration > 0 :
-		if inObstruction :
-			singleObstructionEventList.append(SingleObstruction(currentSeverity, states[x], times[x]))
-			allObstructions.append(singleObstructionEventList)
+	prevState = currentMotionState.GetStateType()
+	nextState = currentMotionState.RunStateMachine(acceleration, speeds[x])
+	if nextState == MotionState.Stopped :
+		currentMotionState = StoppedState
+	elif nextState == MotionState.Accelerating :
+		currentMotionState = AcceleratingState
+	elif nextState == MotionState.Decelerating :
+		currentMotionState = DeceleratingState
+	elif nextState == MotionState.Cruising :
+		currentMotionState = CruisingState
 
-		inObstruction = False
-		currentSeverity = 0
-		singleObstructionEventList = []
-
-	else : # Detecting an obstruction
-		
+	if currentMotionState.GetStateType() == MotionState.Stopped or currentMotionState.GetStateType() == MotionState.Decelerating :
 		currentSeverity += 1
+		obstructionVisionStates.append(states[x])
 
-		if states[x] != lastState or not inObstruction :
-			singleObstructionEventList.append(SingleObstruction(currentSeverity, states[x], times[x]))
+		if prevState != MotionState.Stopped and prevState != MotionState.Decelerating :
+			obstructionStartTime = times[x]
+	else :
+		if prevState == MotionState.Stopped or prevState == MotionState.Decelerating :
+			allObstructions.append(ObstructionPeriod(currentSeverity, obstructionStartTime, times[x], obstructionVisionStates))
+			obstructionVisionStates = []
+			obstructionStartTime = ""
+		currentSeverity = 0
 
-		lastState = states[x]
-
-		if not inObstruction :
-			inObstruction = True
-		
-
-## Print Things
+## Print Results
 for x in range(0, len(allObstructions)) :
-	toPrintStr = ""
-	for y in range(0, len(allObstructions[x])) :
-		toPrintStr += allObstructions[x][y].curTime + ",  " + str(allObstructions[x][y].severity) + "," + str(allObstructions[x][y].state) + ":  "
-		print(toPrintStr)
+	printString = ""
+	printString += allObstructions[x].startTime + " - " + allObstructions[x].endTime
+	printString += " : " + str(allObstructions[x].severity) + " : "
 
+	for y in range(0, len(allObstructions[x].visionStates)) :
+		printString += " " + str(allObstructions[x].visionStates[y])
 
-# Print the State Names from The data
-# for x in range(0, len(BusDataList)) :
-# 	print(BusDataList[x].stateName)
-# 	print(x)
+	print(printString)
+
+	# if acceleration > 0 :
+	# 	if inObstruction :
+	# 		singleObstructionEventList.append(SingleObstruction(currentSeverity, states[x], times[x]))
+	# 		allObstructions.append(singleObstructionEventList)
+
+	# 	inObstruction = False
+	# 	currentSeverity = 0
+	# 	singleObstructionEventList = []
+
+	# else : # Detecting an obstruction
+		
+	# 	currentSeverity += 1
+
+	# 	if states[x] != lastState or not inObstruction :
+	# 		singleObstructionEventList.append(SingleObstruction(currentSeverity, states[x], times[x]))
+
+	# 	lastState = states[x]
+
+	# 	if not inObstruction :
+	# 		inObstruction = True
