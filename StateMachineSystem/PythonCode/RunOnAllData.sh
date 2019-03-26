124d9cbb928f966613482dd83fdad86eb4da698d
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2019-03-26 01:36:02 -0400

    PL-315 Added a test script

---
+++ StateMachineSystem/PythonCode/RunOnAllData.sh
@@ -0,0 +1,7 @@
+python FindStates.py 735Test.csv
+python FindStates.py 800Test.csv
+python FindStates.py 845Test.csv
+python FindStates.py 1000Test.csv
+python FindStates.py 1610Test.csv
+
+python CalculateStats.py
